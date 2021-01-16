160eff8af2738ba99b3cc8163669bf88b26f4bcf
ckaylor33 <kaylor33@hotmail.co.uk>
2021-01-16 20:49:59 +0000

    types in typescript

---
+++ features/types.ts
@@ -0,0 +1,31 @@
+// TYPES
+
+// Everything we can assign to a variable has a type
+
+// PRIMITIVE TYPES
+// - number
+// - boolean
+// - void
+// - undefined
+// - string
+// - symbol
+// - null
+
+// OBJECT TYPES
+// - functions
+// - arrays
+// - classes
+// - objects
+// - or any type we define ourselves
+
+// EXAMPLES
+
+const today = new Date()
+today.getMonth()
+
+const person = {
+  age: 20,
+}
+
+class Color {}
+const red = new Color()
